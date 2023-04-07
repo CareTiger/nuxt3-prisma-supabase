e89c797d7feb150e04cb54767a8afcfa23718601
Ven <CareTiger@users.noreply.github.com>
2023-04-07 16:17:48 -0500

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,11 @@
 		<h1 class="text-center text-4xl">Landing Page</h1>
 	</div>
 </template>
+<script setup>
+const user = useSupabaseUser();
+watchEffect(() => {
+	if (user.value) {
+		navigateTo("/dashboard");
+	}
+});
+</script>
